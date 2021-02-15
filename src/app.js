937d3e458cc2aac12f854aea2c1feba7fd988500
Nicholas Markus <nicxzmiller2@gmail.com>
2021-02-15 15:18:17 +0100

    skeleton

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+import jumboData from './fixtures/jumbo.json'
+import Jumbotron from './components/jumbotron'
+
+ export default function App() {
+  return (
+    <div className="App">
+      <Jumbotron.Container>
+          {jumboData.map((item) => (
+              <Jumbotron key={item.id} direction={item.direction}>
+                  <p>Hello</p>
+              </Jumbotron>
+          ))}
+      </Jumbotron.Container>
+    </div>
+  );
+}
+
---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Inner } from './styles/jumbotron'
+
+export default function Jumbotron({children, direction = 'row', ...restProps}){
+    return(
+        <Inner direction={direction}>
+            <p>Hello</p>
+        </Inner>
+    )
+}
+
+Jumbotron.Container = function JumbotronContainer({children, ...restProps}){
+    return <Container>
+        {children}
+    </Container>
+}
